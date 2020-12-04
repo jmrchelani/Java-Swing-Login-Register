ca4828207c2ecede1bc0be476ec6075fd79ac5d7
Milton <55441239+jmrchelani@users.noreply.github.com>
2020-12-04 20:08:17 +0500

    Add files via upload

---
+++ AccountData.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sw27.simpleloginregister;
+
+/**
+ *
+ * @author rk
+ */
+public class AccountData {
+    public static String name = "None";
+    public static boolean loggedIn = false;
+}
